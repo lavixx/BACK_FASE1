8150c06933c9ae80d5b5d292227ec37cc8630412
lavi & daf <freitaslavinia29@gmail.com>
2026-03-12 20:30:30 -0300

    Add files via upload

---
+++ 001/app.js
@@ -0,0 +1,17 @@
+let nome = 'João';
+console.log(nome);
+
+function adicionar(a,b){
+    return a + b;
+}
+
+console.log(adicionar(10, 12));
+
+// console.log(global);console.log(global);
+
+setTimeout(()=>{
+    console.log("Operação concluída com sucesso");
+},3000);
+
+console.log(__dirname);
+console.log(__filename);
